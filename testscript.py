79124ca6746cded8d17dc1f7841287e22bf6132b
tomg2031 <tomg2031@gmail.com>
2023-12-30 16:57:41 -0600

    Changed testscript.py, testwindow.ui

--- testscript.py
+++ testscript.py
@@ -2,6 +2,9 @@ import sys
 from PySide6 import QtCore, QtGui, QtWidgets
 from PySide6.QtUiTools import QUiLoader
 
+from_directory = ""
+to_directory = ""
+
 loader = QUiLoader()
 app = QtWidgets.QApplication(sys.argv)  # This is a very common line bc of sys
 
@@ -9,21 +12,46 @@ window = loader.load("TestWindow.ui", None)
 # add functionality here
 print(type(window))
 
-my_button = window.findChildren(
+my_buttons = window.findChildren(
     QtWidgets.QPushButton
 )  # underneath this QTwidget called window, I want to find children of all type and return those as a list
+
+for i in my_buttons:
+    print(i)
+
 my_line_edit = window.findChildren(
     QtWidgets.QLineEdit
 )  # now we have a way of grabbing these child elements we can do things to them
-# # print(myButton)
-# # print(my_line_edit[0].objectName())  # you guessed this right ()
 
-my_button[0].clicked.connect(
+for i in my_line_edit:
+    print(i)
+
+my_buttons[0].clicked.connect(
     lambda: button_clicked()
+)  # specifies the element that clicked is under
+
+my_buttons[1].clicked.connect(
+    lambda: get_from_directory()
+)  # specifies the element that clicked is under
+
+my_buttons[2].clicked.connect(
+    lambda: get_to_directory()
 )  # specifies the element that clicked is under
 
 
 # # we have to tell the button what to do when it is clicked
+def get_from_directory():
+    from_directory = QtWidgets.QFileDialog.getExistingDirectory()  # changed this to be able to select Folder in browser. We will implement this into the testWindow.ui when ready to select file origin, destinatiuon
+    print(from_directory)
+    my_line_edit[0].setText(from_directory)
+
+
+def get_to_directory():
+    to_directory = QtWidgets.QFileDialog.getExistingDirectory()  # changed this to be able to select Folder in browser. We will implement this into the testWindow.ui when ready to select file origin, destinatiuon
+    print(to_directory)
+    my_line_edit[1].setText(to_directory)
+
+
 def button_clicked():
     my_text = my_line_edit[
         0
@@ -32,13 +60,5 @@ def button_clicked():
     print(f"Got super cool text from button again: {my_text}")
 
 
-
 window.show()
 app.exec()
-print(my_button)  # this gives you the full list associated with the variable
-print(
-    my_button[0]
-)  # this gives you the individual value in the list at the first index position.
-print("\n")
-print(type(my_button))
-print(type(my_button[0]))
