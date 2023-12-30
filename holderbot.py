87dc1f099827e215f0f55055cc1b3f16475e80b5
tomg2031 <tomg2031@gmail.com>
2023-12-30 17:06:22 -0600

    file name change
    Holderbot so that it can be imported

--- holderbot.py
+++ holderbot.py
@@ -1,11 +1,13 @@
 # import the necessary modules
-import zipfile, os
+import zipfile
+import os
+
 
 class Holderbot:
     def __init__(self):
-        # define the location of the .zip files 
-            # this is merging the end of the file name with the contents per line 18
-                # added end slashjes
+        # define the location of the .zip files
+        # this is merging the end of the file name with the contents per line 18
+        # added end slashjes
         self.zip_files_path = ""
 
         # define the location to extract files
@@ -13,7 +15,6 @@ class Holderbot:
 
         # create a list of .zip files in the folder
         self.zip_files_list = []
-
 
     def unzip(self, from_path, to_path, *args):
         self.zip_files_path = from_path
@@ -23,11 +24,13 @@ class Holderbot:
 
         # loop through the list of .zip files
         for zip_file in self.zip_files_list:
-            if zip_file.endswith('.zip'):
+            if zip_file.endswith(".zip"):
                 # create an instance of the zipfile
                 zf = zipfile.ZipFile(self.zip_files_path + zip_file)
                 # extract the files and skip any duplicates
-                zf.extractall(self.destination_path, pwd=None, members=None) # deleted skip_duplicates=True How to make this work?
+                zf.extractall(
+                    self.destination_path, pwd=None, members=None
+                )  # deleted skip_duplicates=True How to make this work?
                 # close the file instance
                 zf.close()
 
